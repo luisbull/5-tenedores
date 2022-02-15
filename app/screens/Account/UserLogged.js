2a893db5c69bd0287aa578a463d630acc63feac2
luisbull <uk.luistoro@gmail.com>
2022-02-15 04:05:04 +0000

    added functionality to allow user to close sesion:
    User is logged in right after account is created. Therefore the function to allow user to close session and be redirected to account section.

--- app/screens/Account/UserLogged.js
+++ app/screens/Account/UserLogged.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { View, Text} from "react-native";
+import { View, Text, Button} from "react-native";
+import * as firebase from "firebase";
 
 export default function UserLogged() {
     return (
         <View>
             <Text>xUserLoggedx</Text>
+            <Button 
+                title="Cerrar sesion" 
+                onPress={() => firebase.auth().signOut()}
+            />
         </View>
     )
 }
