f9671360e0d252f9eecca8c4ba2a753362365cd6
luisbull <uk.luistoro@gmail.com>
2022-02-16 16:54:13 +0000

    completed Login.js logic + created loginForm.js component:
    After being able to register a user now is possible to login the user

---
+++ app/components/Account/LoginForm.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Input, Icon, Button } from 'react-native-elements';
+import Loading from '../Loading';
+import { validateEmail } from '../../utils/validations';
+import { isEmpty } from 'lodash';
+import * as firebase from 'firebase';
+import { useNavigation } from '@react-navigation/native';
+
+export default function LoginForm(props) {
+
+    const { toastRef } = props;
+
+    const [showPassword, setShowPassword] = useState(false);
+    const [formData, setFormData] = useState(defaultFormValue());
+    const [loading, setLoading] = useState(false);
+    const navigation = useNavigation();
+
+    const onSubmit = () => {
+
+        console.log(formData);
+
+        if (isEmpty(formData.email) || isEmpty(formData.password)){
+            toastRef.current.show("Todos los campos son obligatorios");
+            // console.log('Todos los campos son obligatorios');
+        }
+        else if (!validateEmail(formData.email)){
+            toastRef.current.show("Email no es correcto");
+            // console.log('Email no es correcto');
+        }
+        else {
+            setLoading(true);
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(formData.email, formData.password)
+                .then(response => {
+                    setLoading(false);
+                    navigation.navigate("account")
+                    console.log(response);
+                })
+                .catch(err => {
+                    setLoading(false);
+                    toastRef.current.show("Email o Contrasena estan incorrectos")
+                    console.log(err);
+                })
+            console.log('OK');
+        }
+    }
+
+    const onChange = (e, type) => {
+        setFormData({ ...formData, [type]: e.nativeEvent.text});
+    }
+
+    return (
+        <View style={styles.formContainer}>
+            <Input 
+                placeholder="Correo Electronico"
+                containerStyle={styles.inputForm}
+                onChange={e => onChange(e, "email")}
+                rightIcon={
+                    <Icon 
+                        type='material-community'
+                        name='at'
+                        iconStyle={styles.iconRight}
+                    />
+                }
+            />
+            <Input 
+                placeholder="Constrasena"
+                containerStyle={styles.inputForm}
+                password={true}
+                secureTextEntry={showPassword ? false : true}
+                onChange={e => onChange(e, "password")}
+                rightIcon={
+                    <Icon 
+                        type='material-community'
+                        name={showPassword ? 'eye-off-outline' : 'eye-outline'}
+                        iconStyle={styles.iconRight}
+                        onPress={() => setShowPassword(!showPassword)}
+                    />
+                }
+            />
+            <Button 
+                title="Iniciar sesion"
+                containerStyle={styles.btnContainerLogin}
+                buttonStyle={styles.btnLogin}
+                onPress={onSubmit}
+            />
+            <Loading 
+                isVisible={loading}
+                text="Iniciando sesion"
+            />
+        </View>
+    );
+}
+
+function defaultFormValue() {
+    return {
+        email: "",
+        password: "",
+    };
+}
+
+const styles = StyleSheet.create({
+    formContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30,
+    },
+    inputForm: {
+        width: '100%',
+        marginTop: 30,
+    },
+    btnContainerLogin: {
+        marginTop: 20,
+        width: '95%',
+    },
+    btnLogin: {
+        backgroundColor: '#00a680',
+    },
+    iconRight: {
+        color: '#c1c1c1'
+    }
+})
--- app/screens/Account/Login.js
+++ app/screens/Account/Login.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import { StyleSheet, View, ScrollView, Text, Image } from "react-native";
 import { Divider } from "react-native-elements";
+import Toast from "react-native-easy-toast";
 import { useNavigation } from "@react-navigation/native";
 
+import LoginForm from "../../components/Account/LoginForm";
+
 export default function Login(){
+
+    const toastRef = useRef();
     return(
+
         <ScrollView>
             <Image 
                 source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
@@ -12,11 +18,12 @@ export default function Login(){
                 style={styles.logo}
             />
             <View style={styles.viewContainer}>
-                <Text>Login Form</Text>
+                <LoginForm toastRef={toastRef}/>
                 <CreateAccount />
             </View>
             <Divider style={styles.divider} />
             <Text>Social Login</Text>
+            <Toast ref={toastRef} position="center" opacity={0.9} />
         </ScrollView>
     )
 }
