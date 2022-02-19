c4b42adbf72efad4c6b199e893c3e08306e02d7d
luisbull <uk.luistoro@gmail.com>
2022-02-19 18:26:14 +0000

    Progress with AccountOptinons.js functionality:
    - Improve UI by adding icons and chevrons
    - Making each option clickable.

--- app/components/Account/AccountOptions.js
+++ app/components/Account/AccountOptions.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import { map } from 'lodash';
+import { ListItem, Icon } from 'react-native-elements';
+// import { map } from 'lodash';
 
 export default function AccountOptions(props){
     const { userInfo, toastRef} = props
-    const menuOptions = generateOptions();
-    console.log(userInfo.uid);
-    console.log(menuOptions);
+    // console.log(userInfo.uid);
+    // console.log(menuOptions);
+    const selectedComponent = (key) => {
+        console.log("clicked");
+        console.log(key);
+    }
+    const menuOptions = generateOptions(selectedComponent);
 
     return (
         <View>
-            {map(menuOptions, (menu, index) => {
-                return (
+            {/* {
+                map(menuOptions, (menu, index) => (
                     <ListItem 
                         key={index} 
                         title={menu.title} 
@@ -21,36 +25,73 @@ export default function AccountOptions(props){
                             name: menu.iconNameLeft,
                             color: menu.iconColorLeft
                         }}
-                    />
-                )
-            })}
+                        rightIcon={{
+                            type: menu.iconType,
+                            name: menu.iconNameRight,
+                            color: menu.iconColorRight
+                        }}
+                        containerStyle={styles.menuItems}
+                        onPress={menu.onPress}
+                    >
+                    </ListItem>
+                ))
+            } */}
+            <View>
+                {
+                    menuOptions.map((item, i) => (
+                    <ListItem key={i} style={styles.menuItems} onPress={item.onPress}>
+                        <Icon name={item.iconNameLeft} type={item.iconType} color={item.iconColorLeft} />
+                        <ListItem.Content>
+                        <ListItem.Title>{item.title}</ListItem.Title>
+                        </ListItem.Content>
+                        <ListItem.Chevron color={item.iconColorRight}/>
+                    </ListItem>
+                    ))
+                }
+            </View>
         </View>
+
     )
 }
 
-function generateOptions(){
+function generateOptions(selectedComponent){
     return [
         { 
             title: "Cambiar Nombre",
             iconType: "material-community",
             iconNameLeft: "account-circle",
-            iconColorLeft: "#ccc"
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "#ccc",
+            onPress: () => selectedComponent("DisplayName")
+
         },
         { 
             title: "Cambiar Email",
             iconType: "material-community",
             iconNameLeft: "at",
-            iconColorLeft: "#ccc"
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "#ccc",
+            onPress: () => selectedComponent("Email")
         },
         { 
             title: "Cambiar Contrasena",
             iconType: "material-community",
             iconNameLeft: "lock-reset",
-            iconColorLeft: "#ccc" 
+            iconColorLeft: "#ccc",
+            iconNameRight: "chevron-right",
+            iconColorRight: "#ccc",
+            onPress: () => selectedComponent("Password") 
         }
     ]
 }
 
 const styles = StyleSheet.create({
-
+    menuItems: {
+        borderBottomWidth: 1,
+        // borderTopWidth: 1,
+        borderBottomColor: '#e3e3e3',
+        // borderTopColor: '#e3e3e3'
+    }
 })
