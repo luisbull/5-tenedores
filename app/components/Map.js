0c2313028ccf0dff58cdc85c426e501ffafe8548
luisbull <uk.luistoro@gmail.com>
2022-03-08 23:40:57 +0000

    Map.js component created and added to Restaurant.js component.

---
+++ app/components/Map.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import MapView from 'react-native-maps';
+
+export default function Map(props) {
+    const { location, name, height } = props
+  return (
+    <MapView
+        style={{ height: height, width:"100%" }}
+        initialRegion={location}
+    >
+        <MapView.Marker
+            coordinate={{
+                latitude: location.latitude,
+                longitude: location.longitude
+            }}
+        />
+    </MapView>
+  )
+}
--- app/screens/Restaurants/Restaurant.js
+++ app/screens/Restaurants/Restaurant.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { Rating } from 'react-native-elements';
 import Loading from '../../components/Loading';
 import Carousel from '../../components/Carousel';
+import Map from '../../components/Map';
 
 import { firebaseApp } from '../../utils/firebase';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const db = firebase.firestore(firebaseApp);
 const screenWidth = Dimensions.get("window").width;
@@ -14,6 +17,7 @@ export default function Restaurant(props) {
   const { navigation, route } = props;
   const { id, name} = route.params;
   const [restaurant, setRestaurant] = useState(null);
+  const [rating, setRating] = useState(0);
   
   navigation.setOptions({ title: name });
 
@@ -25,6 +29,7 @@ export default function Restaurant(props) {
         const data = response.data();
         data.id = response.id;
         setRestaurant(data);
+        setRating(data.rating);
       })
   },[]);
 
@@ -37,13 +42,78 @@ export default function Restaurant(props) {
         height={250}
         width={screenWidth}
       />
+      <TitleRestaurant
+        name={restaurant.name}
+        description={restaurant.description}
+        rating={rating}
+      />
+      <RestaurantInfo 
+          location={restaurant.location}
+          name={restaurant.name}
+          address={restaurant.address}
+        />
     </ScrollView>
   )
+}
+
+function TitleRestaurant(props){
+  const { name, description, rating } = props;
+  return (
+    <View style={styles.viewRestaurantTitle}>
+      <View style={{flexDirection: "row" }}>
+        <Text style={styles.nameRestaurant}>{name}</Text>
+        <Rating
+          style={styles.rating}
+          imageSize={20}
+          readonly
+          startingValue={parseFloat(rating)}
+        />
+      </View>
+      <Text style={styles.descriptionRestaurant}>{description}</Text>
+    </View>
+  )
+}
+
+function RestaurantInfo(props){
+  const { location, name, address } = props;
+
+  return (
+    <View style={styles.viewRestaurantInfo}>
+      <Text style={styles.restaurantInfoTitle}>
+        Informacion sobre el restaurante
+      </Text>
+      <Map location={location} name={name} height={100}/>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   viewBody: {
     flex: 1,
     backgroundColor : "#fff"
+  },
+  viewRestaurantTitle: {
+    padding: 15
+  },
+  nameRestaurant: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  descriptionRestaurant: {
+    marginTop: 5,
+    color: "grey"
+  },
+  rating: {
+    position: "absolute",
+    right: 0
+  },
+  viewRestaurantInfo: {
+    margin: 15,
+    marginTop: 25
+  },
+  restaurantInfoTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10
   }
 })
