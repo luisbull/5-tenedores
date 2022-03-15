0396b77b1a244e76da046baf0028231317b80b62
luisbull <uk.luistoro@gmail.com>
2022-03-15 02:05:00 +0000

    - Validation logic added where no empty fields are accepted (rating, title, review ) when receiving restaurant user input.
    - User is notified if there is an error with a toast.

--- app/screens/Restaurants/AddReviewRestaurant.js
+++ app/screens/Restaurants/AddReviewRestaurant.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { AirbnbRating, Button, Input } from 'react-native-elements';
+import Toast from 'react-native-easy-toast';
 
 export default function AddReviewRestaurant(props) {
 
@@ -10,11 +11,21 @@ export default function AddReviewRestaurant(props) {
     const [title, setTitle] = useState("");
     const [review, setReview] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const toastRef = useRef();
 
     const addReview = () => {
-        console.log("rating: ", rating);
-        console.log("title: ", title);
-        console.log("review: ", review);
+        if(!rating) {
+            toastRef.current.show("No has dado ninguna puntuacion");
+        }
+        else if(!title){
+            toastRef.current.show("El titulo es obligatorio");
+        }
+        else if(!review){
+            toastRef.current.show("El comentario es obligatorio");
+        }
+        else {
+            console.log("OK");
+        }
     }
 
     return (
@@ -47,6 +58,11 @@ export default function AddReviewRestaurant(props) {
                     onPress={addReview}
                 />
             </View>
+            <Toast
+                ref={toastRef}
+                position="center"
+                opacity={0.9}
+            />
         </View>
   )
 }
