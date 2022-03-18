7b78133283beb1d0137feffce8cf996c2ff36667
luisbull <uk.luistoro@gmail.com>
2022-03-18 21:21:38 +0000

    favorites screen part 1:
    - fetching array list of logged user favorites restaurants.

--- app/screens/Favorites.js
+++ app/screens/Favorites.js
@@ -1,7 +1,60 @@
-import React from "react";
-import { View, Text} from "react-native";
+import React, { useState, useRef, useCallback } from "react";
+import { StyleSheet, View, Text, FlatList, ActivityIndicator, TouchableOpacity, Alert } from "react-native";
+import { Image, Icon, Button } from 'react-native-elements';
+import { useFocusEffect } from '@react-navigation/native';
+
+import { firebaseApp } from "../utils/firebase";
+import firebase from "firebase";
+import 'firebase/firestore';
+
+const db = firebase.firestore(firebaseApp);
 
 export default function Favorites(){
+
+    const [restaurants, setRestaurants] = useState(null);
+    const [userLogged, setUserLogged] = useState(false);
+
+    console.log(restaurants);
+
+    firebase.auth().onAuthStateChanged((user) => {
+        user ? setUserLogged(true) : setUserLogged(false);
+    });
+
+    useFocusEffect(
+        useCallback(() => {
+            if(userLogged){
+                const idUser = firebase.auth().currentUser.uid;
+                db.collection("favorites")
+                    .where("idUser", "==", idUser)
+                    .get()
+                    .then((response) => {
+                        const idRestaurantsArray = [];
+                        response.forEach((doc) => {
+                            idRestaurantsArray.push(doc.data().idRestaurant)
+                        });
+                        getDataRestaurant(idRestaurantsArray).then((response) => {
+                            const restaurants = [];
+                            response.forEach((doc) => {
+                                const restaurant = doc.data();
+                                restaurant.id = doc.id;
+                                restaurants.push(restaurant);
+                            });
+                            setRestaurants(restaurants)
+                        })
+                    })
+            }
+        }, [userLogged])
+    );
+
+    const getDataRestaurant = (idRestaurantsArray) => {
+        const arrayRestaurants = [];
+        idRestaurantsArray.forEach((idRestaurant) => {
+            const result = db.collection("restaurants").doc(idRestaurant).get();
+            arrayRestaurants.push(result);
+        })
+        return Promise.all(arrayRestaurants);
+    }
+
     return (
         <View>
             <Text>xFavoritesx...</Text>
